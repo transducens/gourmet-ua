e5854d95f9ddb765db059ea5edc04a95d5c071a2
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2019-05-29 16:31:10 +0200

    keep track of losses

--- fairseqmodules/translate_early.py
+++ fairseqmodules/translate_early.py
@@ -3,4 +3,11 @@ from fairseq.tasks import register_task
 
 @register_task('translation_early')
 class TranslationEarlyStopTask(fairseq.tasks.translation.TranslationTask):
-    pass
+    def __init__(self, args, src_dict, tgt_dict):
+        super().__init__(args, src_dict, tgt_dict)
+        self.losses=[]
+
+    def valid_step(self, sample, model, criterion):
+        loss, sample_size, logging_output = super().valid_step(sample, model, criterion)
+        self.losses.append(loss)
+        print(self.losses)
