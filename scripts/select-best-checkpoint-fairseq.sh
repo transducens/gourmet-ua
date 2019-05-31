e0c040a8435df258f77a64efad626b65c8f15f5e
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2019-05-31 18:45:16 +0200

    added script for selecting the best checkpoint

---
+++ scripts/select-best-checkpoint-fairseq.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -euo pipefail
+
+CHECKPOINTDIR="$1"
+DEVSL="$2"
+DATABIN="$3"
+VALIDATESCRIPT="$4"
+TRANSLATEARGS="$5"
+GPUS="$6"
+
+rm -f $CHECKPOINTDIR/scores
+for CP in $CHECKPOINTDIR/checkpoint*.pt ; do
+  # Translate and evaluate
+  SCORE=$( CUDA_VISIBLE_DEVICES=$GPUS fairseq-interactive  --input $DEVSL --path $CP $DATABIN | grep '^H-' | cut -f 3 | $VALIDATESCRIPT )
+  echo "$CP $SCORE" >> $CHECKPOINTDIR/scores
+done
+
+#Find the best checkpoint and symlink
+BESTMODEL=$( LC_ALL=C sort -nr -k2,2 -t ' ' < $CHECKPOINTDIR/scores | head -n 1 | cut -f 1 -d ' ' )
+ln -s $(readlink -f $BESTMODEL) $CHECKPOINTDIR/checkpoint_best_metric.pt
