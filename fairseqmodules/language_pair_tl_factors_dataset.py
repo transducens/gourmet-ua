f9745ce7977452e96c8c5742c8ebeb00026c839b
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2019-06-04 13:06:10 +0200

    created task for TL factors

---
+++ fairseqmodules/language_pair_tl_factors_dataset.py
@@ -0,0 +1,171 @@
+import fairseq.data.LanguagePairDataset
+
+def collate(
+    samples, pad_idx, eos_idx, left_pad_source=True, left_pad_target=False,
+    input_feeding=True,
+):
+    if len(samples) == 0:
+        return {}
+
+    def merge(key, left_pad, move_eos_to_beginning=False):
+        return data_utils.collate_tokens(
+            [s[key] for s in samples],
+            pad_idx, eos_idx, left_pad, move_eos_to_beginning,
+        )
+
+    id = torch.LongTensor([s['id'] for s in samples])
+    src_tokens = merge('source', left_pad=left_pad_source)
+    # sort by descending source length
+    src_lengths = torch.LongTensor([s['source'].numel() for s in samples])
+    src_lengths, sort_order = src_lengths.sort(descending=True)
+    id = id.index_select(0, sort_order)
+    src_tokens = src_tokens.index_select(0, sort_order)
+
+    prev_output_tokens = None
+    target = None
+    if samples[0].get('target', None) is not None:
+        target = merge('target', left_pad=left_pad_target)
+        target = target.index_select(0, sort_order)
+        ntokens = sum(len(s['target']) for s in samples)
+
+        target_factors=merge('target_factors', left_pad=left_pad_target)
+        target_factors = target_factors.index_select(0, sort_order)
+
+        if input_feeding:
+            # we create a shifted version of targets for feeding the
+            # previous output token(s) into the next decoder step
+            prev_output_tokens = merge(
+                'target',
+                left_pad=left_pad_target,
+                move_eos_to_beginning=True,
+            )
+            prev_output_tokens = prev_output_tokens.index_select(0, sort_order)
+
+            prev_output_factors = merge(
+                'target_factors',
+                left_pad=left_pad_target,
+                move_eos_to_beginning=True,
+            )
+            prev_output_factors = prev_output_factors.index_select(0, sort_order)
+    else:
+        ntokens = sum(len(s['source']) for s in samples)
+
+    batch = {
+        'id': id,
+        'nsentences': len(samples),
+        'ntokens': ntokens,
+        'net_input': {
+            'src_tokens': src_tokens,
+            'src_lengths': src_lengths,
+        },
+        'target': target,
+        'target_factors': target_factors
+    }
+    if prev_output_tokens is not None:
+        batch['net_input']['prev_output_tokens'] = prev_output_tokens
+    if prev_output_factors is not None:
+        batch['net_input']['prev_output_factors'] = prev_output_factors
+    return batch
+
+class LanguagePairTLFactorsDataset(fairseq.data.LanguagePairDataset):
+    """
+        Methods not overridden:
+        num_tokens: returns max tokens among src and tgt
+        size: returns tuple (src_size,tgt_size), used to filter with --max-positions
+        ordered_indices: sort indexes by length
+
+    """
+    def __init__(
+        self, src, src_sizes, src_dict,
+        tgt=None, tgt_sizes=None, tgt_dict=None,
+        tgt_factors=None,tgt_factors_sizes=None, tgt_factors_dict=None,
+        left_pad_source=True, left_pad_target=False,
+        max_source_positions=1024, max_target_positions=1024,
+        shuffle=True, input_feeding=True, remove_eos_from_source=False, append_eos_to_target=False,
+    ):
+        super().__init__(src, src_sizes, src_dict,
+        tgt, tgt_sizes, tgt_dict,
+        left_pad_source,left_pad_target,
+        max_source_positions, max_target_positions,
+        shuffle, input_feeding, remove_eos_from_source, append_eos_to_target)
+
+        self.tgt_factors=tgt_factors
+        self.tgt_factors_sizes=tgt_factors_sizes
+        self.tgt_factors_dict=tgt_factors_dict
+
+
+    def __getitem__(self, index):
+        d=super().__getitem__(index)
+        tgt_factors_item =self.tgt_factors[index] if self.tgt_factors is not None else None
+        if self.append_eos_to_target:
+            eos = self.tgt_factors_dict.eos() if self.tgt_factors_dict else self.src_dict.eos()
+            if self.tgt_factors and self.tgt_factors[index][-1] != eos:
+                tgt_factors_item = torch.cat([self.tgt_factors[index], torch.LongTensor([eos])])
+
+        d['target_factors']=tgt_factors_item
+        return d
+
+    def collater(self, samples):
+        """Merge a list of samples to form a mini-batch.
+        Args:
+            samples (List[dict]): samples to collate
+        Returns:
+            dict: a mini-batch with the following keys:
+                - `id` (LongTensor): example IDs in the original input order
+                - `ntokens` (int): total number of tokens in the batch
+                - `net_input` (dict): the input to the Model, containing keys:
+                  - `src_tokens` (LongTensor): a padded 2D Tensor of tokens in
+                    the source sentence of shape `(bsz, src_len)`. Padding will
+                    appear on the left if *left_pad_source* is ``True``.
+                  - `src_lengths` (LongTensor): 1D Tensor of the unpadded
+                    lengths of each source sentence of shape `(bsz)`
+                  - `prev_output_tokens` (LongTensor): a padded 2D Tensor of
+                    tokens in the target sentence, shifted right by one position
+                    for input feeding/teacher forcing, of shape `(bsz,
+                    tgt_len)`. This key will not be present if *input_feeding*
+                    is ``False``. Padding will appear on the left if
+                    *left_pad_target* is ``True``.
+                  - `prev_output_factors` (LongTensor): a padded 2D Tensor of
+                      factors in the target sentence, shifted right by one position
+                      for input feeding/teacher forcing, of shape `(bsz,
+                      tgt_factors_len)`. This key will not be present if *input_feeding*
+                      is ``False``. Padding will appear on the left if
+                      *left_pad_target* is ``True``.
+                - `target` (LongTensor): a padded 2D Tensor of tokens in the
+                  target sentence of shape `(bsz, tgt_len)`. Padding will appear
+                  on the left if *left_pad_target* is ``True``.
+                - `target_factors` (LongTensor): a padded 2D Tensor of factors in the
+                  target sentence of shape `(bsz, tgt_factors_len)`. Padding will appear
+                  on the left if *left_pad_target* is ``True``.
+        """
+        return collate(
+            samples, pad_idx=self.src_dict.pad(), eos_idx=self.src_dict.eos(),
+            left_pad_source=self.left_pad_source, left_pad_target=self.left_pad_target,
+            input_feeding=self.input_feeding,
+        )
+
+    def get_dummy_batch(self, num_tokens, max_positions, src_len=128, tgt_len=128):
+        """Return a dummy batch with a given number of tokens."""
+        src_len, tgt_len = utils.resolve_max_positions(
+            (src_len, tgt_len),
+            max_positions,
+            (self.max_source_positions, self.max_target_positions),
+        )
+        bsz = max(num_tokens // max(src_len, tgt_len), 1)
+        return self.collater([
+            {
+                'id': i,
+                'source': self.src_dict.dummy_sentence(src_len),
+                'target': self.tgt_dict.dummy_sentence(tgt_len) if self.tgt_dict is not None else None,
+                'target_factors':self.tgt_factors_dict.dummy_sentence(tgt_len) if self.tgt_factors_dict is not None else None
+            }
+            for i in range(bsz)
+        ])
+
+    @property
+    def supports_prefetch(self):
+        return super().supports_prefetch() and getattr(self.tgt_factors, 'supports_prefetch', False)
+
+    def prefetch(self, indices):
+        super().prefetch()
+        self.tgt_factors.prefetch(indices)
--- fairseqmodules/translate_tlfactors.py
+++ fairseqmodules/translate_tlfactors.py
@@ -3,9 +3,118 @@ import sys
 from fairseq.tasks import register_task
 from fairseq.meters import AverageMeter
 
-from . import translate_early 
+from . import translate_early
+from . import language_pair_tl_factors_dataset
 
 @register_task('translation_tlfactors')
 class TranslationTLFactorsTask(translate_early.TranslationEarlyStopTask):
-    def __init__(self, args, src_dict, tgt_dict):
+
+    @staticmethod
+    def load_pretrained_model(path, src_dict_path, tgt_dict_path , tgt_factors_dict_path, arg_overrides=None):
+        model = utils.load_checkpoint_to_cpu(path)
+        args = model['args']
+        state_dict = model['model']
+        args = utils.override_model_args(args, arg_overrides)
+        src_dict = Dictionary.load(src_dict_path)
+        tgt_dict = Dictionary.load(tgt_dict_path)
+        tgt_factors_dict = Dictionary.load(tgt_factors_dict_path)
+        assert src_dict.pad() == tgt_dict.pad()
+        assert src_dict.eos() == tgt_dict.eos()
+        assert src_dict.unk() == tgt_dict.unk()
+
+        task = TranslationTLFactorsTask(args, src_dict, tgt_dict,tgt_factors_dict)
+        model = task.build_model(args)
+        model.upgrade_state_dict(state_dict)
+        model.load_state_dict(state_dict, strict=True)
+        return model
+
+    def __init__(self, args, src_dict, tgt_dict, tgt_factors_dict):
         super().__init__(args, src_dict, tgt_dict)
+        self.tgt_factors_dict=tgt_factors_dict
+
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        parent_task= super(TranslationTLFactorsTask, cls).setup_task(args,kwargs)
+        tgt_factors_dict = cls.load_dictionary(os.path.join(args.data[0], 'dict.{}factors.txt'.format(args.target_lang)))
+        return  cls(args, parent_task.src_dict, parent_task.tgt_dict, tgt_factors_dict)
+
+    def load_dataset(self, split, combine=False, **kwargs):
+        """Load a given dataset split.
+        Args:
+            split (str): name of the split (e.g., train, valid, test)
+        """
+
+        def split_exists(split, src, tgt, lang, data_path):
+            filename = os.path.join(data_path, '{}.{}-{}.{}'.format(split, src, tgt, lang))
+            if self.args.raw_text and IndexedRawTextDataset.exists(filename):
+                return True
+            elif not self.args.raw_text and IndexedDataset.exists(filename):
+                return True
+            return False
+
+        def indexed_dataset(path, dictionary):
+            if self.args.raw_text:
+                return IndexedRawTextDataset(path, dictionary)
+            elif IndexedDataset.exists(path):
+                if self.args.lazy_load:
+                    return IndexedDataset(path, fix_lua_indexing=True)
+                else:
+                    return IndexedCachedDataset(path, fix_lua_indexing=True)
+            return None
+
+        src_datasets = []
+        tgt_datasets = []
+        tgt_factors_datasets = []
+
+        data_paths = self.args.data
+
+        for dk, data_path in enumerate(data_paths):
+            for k in itertools.count():
+                split_k = split + (str(k) if k > 0 else '')
+
+                # infer langcode
+                src, tgt = self.args.source_lang, self.args.target_lang
+                if split_exists(split_k, src, tgt, src, data_path):
+                    prefix = os.path.join(data_path, '{}.{}-{}.'.format(split_k, src, tgt))
+                elif split_exists(split_k, tgt, src, src, data_path):
+                    prefix = os.path.join(data_path, '{}.{}-{}.'.format(split_k, tgt, src))
+                else:
+                    if k > 0 or dk > 0:
+                        break
+                    else:
+                        raise FileNotFoundError('Dataset not found: {} ({})'.format(split, data_path))
+
+                src_datasets.append(indexed_dataset(prefix + src, self.src_dict))
+                tgt_datasets.append(indexed_dataset(prefix + tgt, self.tgt_dict))
+                tgt_factors_datasets.append(indexed_dataset(prefix + tgt + "factors", self.tgt_factors_dict))
+
+                print('| {} {} {} examples'.format(data_path, split_k, len(src_datasets[-1])))
+
+                if not combine:
+                    break
+
+        assert len(src_datasets) == len(tgt_datasets)
+
+        if len(src_datasets) == 1:
+            src_dataset, tgt_dataset , tgt_factors_dataset = src_datasets[0], tgt_datasets[0], tgt_factors_datasets[0]
+        else:
+            sample_ratios = [1] * len(src_datasets)
+            sample_ratios[0] = self.args.upsample_primary
+            src_dataset = ConcatDataset(src_datasets, sample_ratios)
+            tgt_dataset = ConcatDataset(tgt_datasets, sample_ratios)
+            tgt_factors_dataset = ConcatDataset(tgt_factors_datasets, sample_ratios)
+
+        self.datasets[split] = language_pair_tl_factors_dataset.LanguagePairTLFactorsDataset(
+            src_dataset, src_dataset.sizes, self.src_dict,
+            tgt_dataset, tgt_dataset.sizes, self.tgt_dict,
+            tgt_factors_dataset, tgt_factors_dataset.sizes, self.tgt_factors_dict,
+            left_pad_source=self.args.left_pad_source,
+            left_pad_target=self.args.left_pad_target,
+            max_source_positions=self.args.max_source_positions,
+            max_target_positions=self.args.max_target_positions,
+        )
+
+     @property
+    def target_factors_dictionary(self):
+        """Return the target factors :class:`~fairseq.data.Dictionary`."""
+        return self.tgt_factors_dict
