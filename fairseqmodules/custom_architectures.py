0f16643bfe2e04b98757a1d8f58bd254d7789e2f
Victor Manuel Sanchez Cartagena <vmsanchez@abaco.dlsi.ua.es>
2019-06-05 10:57:12 +0000

    fixed architecture

--- fairseqmodules/custom_architectures.py
+++ fairseqmodules/custom_architectures.py
@@ -5,9 +5,6 @@ def lstm_wmt2017(args):
     args.decoder_hidden_size = getattr(args, 'decoder_hidden_size', 1024)
     args.encoder_hidden_size = getattr(args, 'encoder_hidden_size', 1024)
     args.encoder_bidirectional= getattr(args, 'encoder_bidirectional', True)
-    args.share_input_output_embed=getattr(args, 'share_input_output_embed', True) 
-    args.max_source_positions= getattr(args, 'max_source_positions', 100)
-    args.max_target_positions= getattr(args, 'max_target_positions', 100)
-#    args.= getattr(args, '', )
-#    args.= getattr(args, '', )
+    #The default=False in LSTMModel add_args breaks the getattr approach
+    args.share_decoder_input_output_embed = True #getattr(args, 'share_decoder_input_output_embed', True)
 
