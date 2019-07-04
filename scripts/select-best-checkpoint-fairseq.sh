74cc6128957102ebe7bb1a08eda901cab5040e7e
Victor Manuel Sanchez Cartagena <vmsanchez@abaco.dlsi.ua.es>
2019-07-04 08:14:31 +0000

    Selection of best checkpoint script keeps partial translations and names strategy

--- scripts/select-best-checkpoint-fairseq.sh
+++ scripts/select-best-checkpoint-fairseq.sh
@@ -8,13 +8,17 @@ VALIDATESCRIPT="$4"
 TRANSLATEARGS="$5"
 GPUS="$6"
 
-rm -f $CHECKPOINTDIR/scores
+NAME="$7"
+
+SCORESFILE="scores$NAME"
+
+rm -f $CHECKPOINTDIR/$SCORESFILE
 for CP in $CHECKPOINTDIR/checkpoint*.pt ; do
   # Translate and evaluate
-  SCORE=$( CUDA_VISIBLE_DEVICES=$GPUS fairseq-interactive $TRANSLATEARGS  --input $DEVSL --path $CP $DATABIN | grep '^H-' | cut -f 3 | $VALIDATESCRIPT )
-  echo "$CP $SCORE" >> $CHECKPOINTDIR/scores
+  SCORE=$( CUDA_VISIBLE_DEVICES=$GPUS fairseq-interactive $TRANSLATEARGS  --input $DEVSL --path $CP $DATABIN | grep '^H-' | cut -f 3 | tee $CP.output$NAME | $VALIDATESCRIPT )
+  echo "$CP $SCORE" >> $CHECKPOINTDIR/scores$NAME
 done
 
 #Find the best checkpoint and symlink
-BESTMODEL=$( LC_ALL=C sort -nr -k2,2 -t ' ' < $CHECKPOINTDIR/scores | head -n 1 | cut -f 1 -d ' ' )
-ln -s $(readlink -f $BESTMODEL) $CHECKPOINTDIR/checkpoint_best_metric.pt
+BESTMODEL=$( LC_ALL=C sort -nr -k2,2 -t ' ' < $CHECKPOINTDIR/scores$NAME | head -n 1 | cut -f 1 -d ' ' )
+ln -s $(readlink -f $BESTMODEL) $CHECKPOINTDIR/checkpoint_best_metric$NAME.pt
