35958f5bf9a9b5226f9fb61a3af3d5341bc93888
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2019-07-26 15:47:58 +0200

    Select best surface form perplexity

---
+++ scripts/select-best-checkpoint-best-sf-perplexity.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MODELDIR="$1"
+
+BEST_UPDATE=$(grep 'valid on' $MODELDIR/../train.log | cut -f 7,9 -d '|' | grep ' loss_a' | LC_ALL=C sort -n -k 5,5  | head -n 1 | cut -f 3 -d  ' ')
+echo "Copying checkpoint $BEST_UPDATE to checkpoint_last with comand: cp $MODELDIR/checkpoint_*_$BEST_UPDATE.pt $MODELDIR/checkpoint_last.pt"
+cp $MODELDIR/checkpoint_*_$BEST_UPDATE.pt $MODELDIR/checkpoint_last.pt
