9bf334a7f06cb3ce464a474828d4069eaccceef5
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2020-02-03 15:19:37 +0100

    Removed old code

--- scripts/extract-interleaving-tags.py
+++
@@ -1,8 +0,0 @@
-import sys
-IL_PREFIX="interleaved_"
-for line in sys.stdin:
-    line=line.rstrip("\n")
-    toks=line.split(" ")
-    il=[t for t in toks if t.startswith(IL_PREFIX)]
-    il_clean=[ t[len(IL_PREFIX):] for t in il]
-    print(" ".join(il_clean))
